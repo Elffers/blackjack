07617fa6c7beac067b0e92cc29a746c4f6b1ae59
Hsing-Hui and Kristina <dev+hsing-hui+kristina@carezone.com>
2015-06-17 21:30:15 -0700

    score works

--- blackjack.rb
+++ blackjack.rb
@@ -1,24 +1,37 @@
 class BlackJack
-  attr_accessor :c_1, :c_2
+  attr_reader :hand
+
+  SCORES = {
+    "J" => 10,
+    "Q" => 10,
+    "K" => 10,
+    "A" => 11
+  }
+
+  BLACKJACK = 21
 
   def initialize(c_1, c_2)
-    @c_1 = value(c_1)
-    @c_2 = value(c_2)
+    @hand = [c_1, c_2]
   end
 
   def score
-    @c_1 + @c_2
+    current_score = @hand.reduce(0){ |score, card| score + value(card) }
+    aces_count = @hand.count("A")
+    while (current_score > BLACKJACK) && aces_count > 0
+      current_score -= 10
+      aces_count -= 1
+    end
+    current_score
   end
+
+  def deal(c)
+    @hand << c
+  end
+
+  private
 
   def value(c)
-    if c.class == Integer
-      return c
-    else
-      if c != "A"
-        return 10
-      end
-    end
+    SCORES[c] || c
   end
-
 end
 
--- blackjack_spec.rb
+++ blackjack_spec.rb
@@ -3,14 +3,37 @@ require_relative 'blackjack'
 describe BlackJack do
   let(:blackjack) { BlackJack.new(10, 5) }
   let(:blackjack2) { BlackJack.new(10, "J") }
+  let(:blackjack3) { BlackJack.new("A", 7) }
 
-  describe '#score' do
+  describe '#score_hand' do
     it "scores the cards" do
       expect(blackjack.score).to eq 15
     end
 
     it "scores face cards" do
       expect(blackjack2.score).to eq 20
+    end
+
+    it "scores with a high Ace" do
+      expect(blackjack3.score).to eq 18
+    end
+
+    it "scores with a low Ace" do
+      blackjack2.deal("A")
+      expect(blackjack2.score).to eq 21
+    end
+
+    it "scores all the cards" do
+      blackjack.deal 5
+      expect(blackjack.score).to eq 20
+    end
+  end
+
+  describe "#deal" do
+    let(:blackjack) { BlackJack.new(10, 5) }
+    it "adds a new card to the hand" do
+      blackjack.deal 5
+      expect(blackjack.hand).to eq [10, 5, 5]
     end
   end
 end
